31105a35bc9a53480b5fe81a44aa043716db4f10
volcano <412272566@qq.com>
2018-12-18 20:37:55 +0800

    idea的更改提交到本地仓库

--- git_01/src/cn/itcast/GitTest.java
+++ git_01/src/cn/itcast/GitTest.java
@@ -3,5 +3,8 @@ package cn.itcast;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I love you");
+        }
     }
 }
